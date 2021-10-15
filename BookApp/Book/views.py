128e35317ea80486dcd11f75b671994f9f627f17
SadLoneWolf <djordjeandjic99@gmail.com>
2021-10-15 18:04:18 +0300

    ! Migrations and admin panel

--- BookApp/Book/views.py
+++ BookApp/Book/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .models import Book
 from .forms import BookForm
 from django.urls import reverse_lazy
+from django.views.decorators.csrf import *
 
 # Create your views here.
 def read(request):
@@ -9,12 +10,19 @@ def read(request):
     context = {"book":books}
     return render (request,"Book/book_list.html",context)
 
+@csrf_protect
 def create(request): 
     form = BookForm()
     if request.method == "POST":
+        form = BookForm(request.POST)
         if form.is_valid():
+            book = Book()
+            book.title = form.cleaned_data["title"]
+            book.author = form.cleaned_data["author"]
+            book.comment = form.cleaned_data["comment"]
             form.save()
-            return redirect(read)
+            book.save()
+            return redirect('/')
     else:
         context = {"form":form}
         return render(request,"Book/book_create.html",context)
